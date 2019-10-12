40b2fa4f13721fb53467cdbf7a662a27474a0bb7
RedHenDev <redsheepdev@gmail.com>
2019-10-12 12:25:28 +0100

    Looks batmanny and pink

--- plotFunction/plotRDPalg.py
+++ plotFunction/plotRDPalg.py
@@ -1,16 +1,13 @@
 
 
 # So, the aim here is to
-# define the RDP algorithm
+# define the Ramer-Douglas-Peucker algorithm
 # and then, to begin with, output
 # return values as each new input
 # is given by the user.
 
-""" plotFunc 4.0 """
+""" RDP algorithm 1.0, built on plotFunc 4.0 """
 
-""" Here, we aim to try different function equations. """
-
-# Math module used for floor() in version 1.0.
 from math import *
 import time
 from graphics import *
@@ -21,24 +18,28 @@ x = float(1)
 newX = float(0)
 # Our current increment value.
 iX = float(1)
+# Previous point (x,y).
+prevP = Point(0,0)
 
 # Our integer-cum-bool for while looping indefinitely.
 looping = 1
 
+# Create the graphics window and render basic cross-hair.
 win = GraphWin("",700,700)
-win.setBackground(color_rgb(0,101,101))
+win.setBackground(color_rgb(0,0,0))
 yLine = Line(Point(350,0), Point(350,700))
 xLine = Line(Point(0,350), Point(700,350))
-yLine.setOutline("gray")
+yLine.setWidth(1)
+xLine.setWidth(1)
+yLine.setOutline("yellow")
 xLine.setOutline("yellow")
 yLine.draw(win)
 xLine.draw(win)
-cir = Circle(Point(350,350), 42)
-cir.setOutline("black")
+cir = Circle(Point(350,350), 9)
+cir.setOutline("white")
 cir.draw(win)
 
-# Note that function definition has to appear before it is called in python.
-# Does this have something to do with not having 'hoisting' like javascript?
+# Here's our Ramer-Douglas-Peucker function.
 def func(_x):
 
     e = exp(-_x)
@@ -51,7 +52,7 @@ def func(_x):
 while looping != 0:
 
     if looping == 1:
-        x = input("\nWhat x to input?   fn(x): ")
+        x = input("\nWhat x to input?   f(x): ")
 
         try:
             tX = int(x)
@@ -60,8 +61,8 @@ while looping != 0:
                 tX = float(x)
             except:
                 if x == "":
-                    x = float(-50)
-                    print("\nx set to default: -50")
+                    x = float(-1)
+                    print("\nx set to default: -1")
                     # If no input value entered, just Enter key,
                     # then assume default value.
                 else:
@@ -70,7 +71,7 @@ while looping != 0:
 
         newX = float(x)
         # OK, we have now taken initial X value.
-        xi = input("\nWhat increment (n) to x?  fn(x+n): ")
+        xi = input("\nWhat increment (n) to x?  f(x+n): ")
         # If nothing entered, default to 1 for inc value.
           
 
@@ -97,12 +98,12 @@ while looping != 0:
                     # If no input value entered, just Enter key,
                     # then assume increment value is 1.
                     # I.e, if  looped through more than once.
-                    iX = float(0.7)
-                    print ("\nDefault value for increment set: 0.7")
+                    iX = float(0.01)
+                    print ("\nDefault value for increment set: 0.01")
                 elif xi != "":
-                    print("Sorry: invalid input, buddy. Default value set for increment: 0.7")
+                    print("Sorry: invalid input, buddy. Default value set for increment: 0.01")
                     # Assign x the value zero, so as to unaffect progress.
-                    iX = float(0.7)
+                    iX = float(0.01)
 
     # Have we looped through at least once?
     if looping == 2:
@@ -119,23 +120,34 @@ while looping != 0:
     prevY = func(float(newX - float(iX)))
     y = func(float(newX))
     print("x: " + str(float(newX)) + " y: " + str(y))
-    scalar = 64
-    pxy = Point(350+float(newX)*scalar, 350-(func(float(newX))*scalar))
+    scalar = 350
+    scalarY = scalar*0.2
+    
+    pxy = Point(350+float(newX)*scalar, 350-(func(float(newX))*scalarY))
     
     if (prevY < 0 and y > 0) or (prevY > 0 and y < 0): print(newX)
+
+    if newX > -1:
+        ly = Line(prevP, pxy)
+        ly.setWidth(12)
+        ly.setOutline(color_rgb(255,0,255))
+        ly.draw(win)
+
+    # record previous point. Used to draw lines between points.
+    prevP = pxy
     
-    pCir = Circle(pxy, 1.5)
-    pCir.setOutline("white")
-    pCir.setFill("white")
-    pCir.draw(win)
+    #pCir = Circle(pxy, 0.5)
+    #pCir.setOutline("white")
+    #pCir.setFill("white")
+    #pCir.draw(win)
+    
     # Now how to pause a short time?
-    time.sleep(0.01)
-    if win.checkKey() == "x" or win.checkKey() == "X" or win.checkKey() == "q" or \
-    win.checkKey() == "Q":
+    # time.sleep(0.00001)
+    if win.checkKey() == "q":
         win.close()
         break
 
-    if newX > 42: break
+    if newX * scalar > 349: break
 
 # If we are here, then while loop has terminated, so exit.
 print("You have exited :) Thanks for your maths etc.")
