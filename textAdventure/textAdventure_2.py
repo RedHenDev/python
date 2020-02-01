9d4b0c6c6c689dcc15b7a411451435cab42c17de
RedHenDev <redsheepdev@gmail.com>
2020-02-01 22:57:26 +0000

    Text adventure kinda working

--- textAdventure/textAdventure_2.py
+++ textAdventure/textAdventure_2.py
@@ -51,8 +51,8 @@ def assembleQuestion(question, ans1, ans2):
         print("")
         ua = input(question + "\n" + ans1 + " or " + ans2 + "? > ").strip().lower()
 
-        # Return user's choice as integer, 1 or 2.
-        # If they enter incoherent input, prompt with alternative and call function recursively.
+        # Return user's choice as string.
+        # If they enter incoherent input, prompt with available answers.
         if ua == ans1: return ans1
         elif ua == ans2: return ans2
         elif ua == "s": return "s"
@@ -60,12 +60,47 @@ def assembleQuestion(question, ans1, ans2):
         else:
                 print("\n" + "Pardon? Choose either " + ans1 + " or " + ans2 + ". Thank you.\n" )
                 return 3
-                
+
+
+# Assemble question, based on level.
+# May need corresponding function that handles the answers given.
+def findQuestion():
+        global level
+        if level == 1:
+                return assembleQuestion("The forest is dark and quiet. Move on, or retreat for supplies?",
+                             "move", "retreat")
+        elif level == 2:
+                return assembleQuestion("Quick feet! You've made it far already. Press on to the heart of the forest?",
+                             "yes", "no")
+        elif level == 3:
+                return assembleQuestion("You have packed well. But there's space for one more item: torch or cloak?",
+                             "torch", "cloak")
+        elif level == 4:
+                return assembleQuestion("The darkness grows. You slow down. You hear a scream. Run towards, or away?",
+                             "towards", "away")
+        elif level == 5:
+                return assembleQuestion("You find a strange opening in the trees. Enter or walk on?",
+                             "enter", "walk")
+
+# Looks at answer given (_a) and returns new level.
+def junctionBox(_a):
+        global level
+        if level == 1 and _a == "move": return 2
+        elif level == 1 and _a == "retreat": return 3
+        elif level == 2 and _a == "yes": return 4
+        elif level == 2 and _a == "no": return 5
+        elif level == 3: return 5
+        elif level == 5 and _a == "walk": return 2
+        elif level == 4 and _a == "away": return 5
+        elif level == 4 and _a == "towards": return 3
+        elif level == 5 and _a == "enter": return 1
+        else: return level
+
 def damage():
         global health
         health -= randint(1,15)
 
-level = randint(1,3)
+level = 1
 printStatus()
 
 # Our game loop.
@@ -74,16 +109,18 @@ while alive:
         if health <= 0:
                 alive = False
                 break
-        a = assembleQuestion("The forest is dark and quiet. Move on, or retreat for supplies?",
-                             "move", "retreat")
+        a = findQuestion()
         # a = input(getQuestion()).strip()
         if a != "x" and a != 3 and a!= "s":
                 print("You chose to", a)
                 print("")
         if a == "x": alive = False
         elif a == "s":
-                level = randint(1,3)
                 printStatus()
+        # Determine new level based on answer.
+        else: level = junctionBox(a)
         
 
-print("You have exited the game.")
+printStatus()
+
+print("You are dead, or have exited the game.")
