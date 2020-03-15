9050753bc72c6d7898907c6cd3fb75f8ad06158d
RedHenDev <redsheepdev@gmail.com>
2020-03-15 20:40:47 +0000

    Mic input
    Pip3 installed bumpy and sounddevice

---
+++ soundPython/micRead.py
@@ -0,0 +1,11 @@
+# Print out realtime audio volume as ascii bars
+
+import sounddevice as sd
+import numpy as np
+
+def print_sound(indata, outdata, frames, time, status):
+    volume_norm = np.linalg.norm(indata)*10
+    print ("|" * int(volume_norm))
+
+with sd.Stream(callback=print_sound):
+    sd.sleep(10000)
