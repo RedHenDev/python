b133674f44eec9761e01d1e23147d4e501fb4ad3
RedHenDev <neobn@hotmail.com>
2021-05-03 14:32:04 +0100

    experimenting with cubes

--- ursina dev/surlina_system.py
+++ ursina dev/surlina_system.py
@@ -23,13 +23,14 @@ def update():
     x += 0.01
     sun.y = (nn.sin(x) * 0.7)
 
-    for p in planets:
-        p.rotation()
-        p.bob()
+    #for p in planets:
+      #  p.rotation()
+        #p.bob()
 
 class Planet:
     def __init__(this, radius, colour):
-        this.ent = Entity(model="sphere",color=colour,scale=radius,texture='2k_moon')
+        this.ent = Entity(model="cube",color=colour,
+                          scale=radius,texture='2k_moon')
         this.rotSpeed = ra.randint(20,100) * 0.1
 
     def rotation(this):
@@ -45,7 +46,8 @@ app = Ursina()
 
 window.color = color.rgb(184,0,184)
 
-sun = Entity(model="sphere",color=color.rgba(222,200,0,200),scale=12,texture='2k_sun')
+sun = Entity(model="sphere",color=color.rgba(222,200,0,200),scale=12,
+             texture='2k_sun')
 #ground = Entity(model="cube",color=color.green,scale=(1000,1,1000),collider='mesh')
 #ground.y = -10
 
