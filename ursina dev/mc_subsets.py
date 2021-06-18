b27f3b2f47291409491a5c6122434771806e0284
RedHenDev <neobn@hotmail.com>
2021-06-18 23:58:34 +0100

    Update mc_subsets.py

--- ursina dev/mc_subsets.py
+++ ursina dev/mc_subsets.py
@@ -30,11 +30,11 @@ scene.fog_color = color.rgb(0,222,200)
 
 subject = FirstPersonController()
 subject.cursor.visible = False
-subject.gravity = 0.5
-subject.speed = 10
+subject.gravity = 0.01
+subject.speed = 5
 
 #  Original position of subject etc.
-subject.y = 16
+subject.y = 32
 subject.x = 0
 subject.z = 0
 
@@ -45,11 +45,13 @@ prevZ = 0
 
 # Perlin noise setup.
 noise = PerlinNoise(octaves=4,seed=1988)
+amp = 32
+freq = 104
 
 jojo = Entity(  model='cube',
                 collider=None,
                 color=color.rgba(0,101,101,64))
-buildMode = 1 # Toggle with 'f'
+buildMode = -1 # Toggle with 'f'
 
 def projectBuilder():
     jojo.position = subject.position + Vec3(camera.forward * 2)
@@ -78,10 +80,10 @@ def input(key):
             destroy(e)
 
 def update():
-    global prevX, prevZ, ghostTime
+    global prevX, prevZ, ghostTime, ghostDone
     if  nn.abs(nn.floor(subject.z)-nn.floor(prevZ)) >= 2 or \
         nn.abs(nn.floor(subject.x)-nn.floor(prevX)) >= 2 or \
-        nn.abs(subject.x-subject.x)+nn.abs(subject.z-subject.z) >= 2:
+        nn.abs(subject.x-prevX)+nn.abs(subject.z-prevZ) >= 2:
         prevX = subject.x
         prevZ = subject.z
         generateShell()
@@ -91,11 +93,11 @@ def update():
     else: jojo.y = -99 # Hide jojo.
 
     # Continue to build 10K terrain!
-    if time.time() - ghostTime > 5:
-        subject.gravity = 0 # Prevent subject glitching through terrain.
-        subject.y += 0.2
+    if ghostDone == False and time.time() - ghostTime > 0.5:
+        # subject.gravity = 0 # Prevent subject glitching through terrain.
+        # subject.y += 0.2
         generateGhost()
-        subject.gravity = 0.5
+        # subject.gravity = 0.5
         ghostTime = time.time() # Update timeStamp AFTER generation!
 
 
@@ -115,15 +117,28 @@ for i in range(blocksWidth*blocksWidth):
     bub.disable()
     blocks.append(bub)
 
+# Single block entity used to create strip.
+# If inefficient, will try with a list of 5 blocks instead.
+gblocks = []
+terrainWidth = 100
+subWidth = 50
+# Ghost-terrain.
+ghost = Entity(model=None,collider=None)
+ghost.disable()
+for i in range(subWidth):
+    bud = Entity(model='cube',collider=None,
+            visible=False,parent=ghost)
+    bub.disable()
+    gblocks.append(bud)
+
 # Terrain data.
 urizenData = []
-terrainWidth = 100
 for i in range (terrainWidth*terrainWidth):
     # NB. This was reversed in previous non-subset version.
     x = nn.floor(i/terrainWidth)
     z = nn.floor(i%terrainWidth)
-    freq = 64
-    amp = 12
+    # freq = 64
+    # amp = 12
     y = (noise([x/freq,z/freq])* amp)
     y = nn.floor(y)
     urizenData.append(y)
@@ -152,30 +167,19 @@ def generateShell():
     shell.x = nn.floor(subject.x + -((blocksWidth-2.5)*0.5))
     shell.z = nn.floor(subject.z + -((blocksWidth-2.5)*0.5))
 
-#  Let's gooooo!
-generateShell()
-
-# Ghost-terrain.
-ghost = Entity( model=None,
-                texture='grass_mono.png',
-                color=color.rgb(222,0,0),
-                collider=None)
-# Single block entity used to create strip.
-# If inefficient, will try with a list of 5 blocks instead.
-gblocks = []
-for i in range(terrainWidth*5):
-    bud = Entity(model='cube',collider=None,visible=False,parent=ghost)
-    bub.disable()
-    gblocks.append(bud)
-
 ci = 0 # Current index. I.e., of created mesh-block so far.
 ghostTime = 0 # Time stamp for when to generate new subset.
+ghostDone = False
 def generateGhost():
-    global ci, terrainWidth, blocksWidth, block
-    if ci >= len(urizenData): return
+    global ci, terrainWidth, subWidth
+    global ghostDone
+    if ci >= len(urizenData)-1: 
+        ghost.combine(auto_destroy=False)
+        ghostDone = True
+        return
     # Iterate from current index to index + (blocksWidth-1).
     # So, not a 5*5 but a little strip.
-    for i in range(ci,ci+terrainWidth*5):
+    for i in range(ci,ci+subWidth):
         x = gblocks[i-ci].x = nn.floor(i/terrainWidth)
         z = gblocks[i-ci].z = nn.floor(i%terrainWidth)
         # Check index. If out of range, return to default
@@ -183,11 +187,24 @@ def generateGhost():
         indi = int((x*terrainWidth)+z)
         
         gblocks[i-ci].y = urizenData[indi]
-
+        # nn.floor(noise([x/freq,z/freq])* amp)
+        gblocks[i-ci].collider=None
+        gblocks[i-ci].disable()
         
-    # Add the block to the ghost's mesh.
-    ghost.combine(auto_destroy=False)
-    ci += terrainWidth*5 # Iterate to new index ready for next subset.
+    # Add the block(s) to the ghost's mesh.
+    if ci >= len(urizenData)-1: 
+        ghost.combine(auto_destroy=True)
+        ghostDone = True
+    else: 
+        ghost.combine(auto_destroy=False)
+        ci += subWidth # Iterate to new index ready for next subset.
+    ghost.collider=None
     # All done!
+#  Let's gooooo!
+generateShell()
+# ghost.combine(auto_destroy=True)
+ghost.texture='grass_mono.png'
+ghost.color=color.rgb(222,0,0)
+ghost.collider=None
 
 app.run()
