a2f6443ee84fb567d58a819ed369d5f9f67ea181
RedHenDev <neobn@hotmail.com>
2021-06-23 15:43:44 +0100

    efficiency update

--- ursina dev/python minecraft/subSwirl.py
+++ ursina dev/python minecraft/subSwirl.py
@@ -1,10 +1,18 @@
 """
 Minecraft in Python, with Ursina.
 New idea for a 'subSwirl' system: June 22 2021.
+Working! 23 June 2021 :)
+
+We could also generate a load of subsets at start -- to
+prevent need of generating these entities on the fly.
+This would also mean we have to be a bit more sophisticated
+when combining new subsets into terrain mesh...
+
 """
 
+from operator import sub
 from random import randrange
-from ursina import *
+from ursina import * 
 from ursina.prefabs.first_person_controller import FirstPersonController
 from numpy import floor
 from numpy import abs
@@ -30,7 +38,7 @@ def input(key):
     if key == 'g': generateSubswirl()
 
 def update():
-    global prevZ, prevX, prevTime
+    global prevZ, prevX, prevTime, subsets, terrainArea
     if  abs(subject.z - prevZ) > 1 or \
         abs(subject.x - prevX) > 1:
         generateShell()
@@ -38,23 +46,33 @@ def update():
     # Safety net, in case of glitching through terrain.
     if subject.y < -amp:
         subject.y = floor((noise([subject.x/freq,
-        subject.z/freq]))*amp)+2
+        subject.z/freq]))*amp)+3
 
-    if time.time() - prevTime > 0.33:
+    if time.time() - prevTime > 0.5:
         prevTime = time.time()
-        # generateSubswirl()
+        generateSubswirl()
+        if currentSubset == terrainArea-1:
+            finishTerrain()
 
-noise = PerlinNoise(octaves=4,seed=2021)
+noise = PerlinNoise(octaves=3,seed=2021)
 amp = 32
 freq = 100
 terrain = Entity(model=None,collider=None)
-terrainWidth = 200
-subWidth = 8
+terrainArea = 100
+subWidth = 5
 subArea = subWidth*subWidth
 subsets = []
 subCubes = []
-sci = 0 # subCube index.
 currentSubset = 0
+
+
+# Generate all subsets now, before running app, so that
+# we run more smoothly, not having to instantiate entities
+# on the fly.
+for i in range(terrainArea):
+    sub = Entity(model=None,collider=None)
+    sub.disable()
+    subsets.append(sub)
 
 # For new position of subset.
 currentVec = 0
@@ -72,10 +90,10 @@ swirlVecs = [
 
 def generateSubswirl():
     global currentVec, iterations, changes, subArea
-    global subPos, toIterate, swirlVec
+    global subPos, toIterate, swirlVecs, currentSubset
 
-    sub = Entity(model=None,parent=terrain)
-    subsets.append(sub)
+    # sub = Entity(model=None,parent=terrain)
+    # subsets.append(sub)
 
     # Translate position of subset, according to
     # current vector.
@@ -86,14 +104,17 @@ def generateSubswirl():
         x = subCubes[i].x = floor(i/subWidth) + subPos.x
         z = subCubes[i].z = floor(i%subWidth) + subPos.y
         y = subCubes[i].y = floor((noise([x/freq,z/freq]))*amp)
-        subCubes[i].parent = subsets[-1]
-        b = randrange(188,244)
-        subCubes[i].color = color.rgb(0,0,b)
+        subCubes[i].parent = subsets[currentSubset]
+        r = randrange(142,244)
+        subCubes[i].color = color.rgb(r,0,0)
         subCubes[i].visible = False
     
-    subsets[-1].combine(auto_destroy=False)
-    subsets[-1].texture = grassStrokeTex
-    
+    subsets[currentSubset].texture = monoTex
+    subsets[currentSubset].combine(auto_destroy=False)
+    subsets[currentSubset].parent = terrain
+
+    currentSubset+=1
+
     # Co-ordinate new vector by iteration around swirl.
     iterations+=1
     if iterations == toIterate:
@@ -106,60 +127,29 @@ def generateSubswirl():
             changes=0
             toIterate+=1
 
-    
-
 # Instantiate our 'ghost' subset cubes.
 for i in range(subArea):
     bud = Entity(model='cube')
+    bud.disable()
     subCubes.append(bud)
     
 
-# Instantiate our empty subsets.
-# for i in range(int((terrainWidth*terrainWidth)/subWidth)):
-#     bud = Entity(model=None)
-#     bud.parent = terrain
-#     subsets.append(bud)
-
-# def generateSubset():
-#     global sci, currentSubset, freq, amp
-#     if currentSubset >= len(subsets): 
-#         finishTerrain()
-#         return
-#     for i in range(subWidth):
-#         x = subCubes[i].x = floor((i+sci)/terrainWidth)
-#         z = subCubes[i].z = floor((i+sci)%terrainWidth)
-#         y = subCubes[i].y = floor((noise([x/freq,z/freq]))*amp)
-#         subCubes[i].parent = subsets[currentSubset]
-#         b = randrange(188,244)
-#         subCubes[i].color = color.rgb(0,0,b)
-#         subCubes[i].visible = False
-    
-#     subsets[currentSubset].combine(auto_destroy=False)
-#     subsets[currentSubset].texture = grassStrokeTex
-#     sci += subWidth
-#     currentSubset += 1
-
 terrainFinished = False
+terrain.texture = grassStrokeTex
 def finishTerrain():
-    global terrainFinished
-    if terrainFinished==True: return
-    terrain.combine()
-    terrainFinished = True
-    subject.y = 64
-    terrain.texture = monoTex
-
-
-# for i in range(terrainWidth*terrainWidth):
-#     bud = Entity(model='cube',color=color.green)
-#     bud.x = floor(i/terrainWidth)
-#     bud.z = floor(i%terrainWidth)
-#     bud.y = floor((noise([bud.x/freq,bud.z/freq]))*amp)
-#     bud.parent = terrain
-
-# terrain.combine()
-# terrain.collider = 'mesh'
-# terrain.texture = grassStrokeTex
-
+    global terrainFinished, subsets, terrain, subCubes
+    global currentSubset
+    
+    # for sc in subCubes:
+    #     sc.parent = scene
+    
+    terrain.combine(auto_destroy=False)
+    
+    # for ss in subsets:
+        # ss.model = None
+        # ss.parent = scene
+    currentSubset=0
+    
 shellies = []
 shellWidth = 3
 for i in range(shellWidth*shellWidth):
@@ -178,9 +168,9 @@ def generateShell():
 
 subject = FirstPersonController()
 subject.cursor.visible = False
-subject.gravity = 0.0
+subject.gravity = 0.5
 subject.x = subject.z = 0
-subject.y = amp+3 * 2
+subject.y = amp+3
 prevZ = subject.z
 prevX = subject.x
 
