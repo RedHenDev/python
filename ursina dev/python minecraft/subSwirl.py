f834ce442682cf7a2640a2e23487739f1708fdf2
RedHenDev <neobn@hotmail.com>
2021-06-23 20:01:57 +0100

    faster generation of terrain

--- ursina dev/python minecraft/subSwirl.py
+++ ursina dev/python minecraft/subSwirl.py
@@ -7,6 +7,8 @@ We could also generate a load of subsets at start -- to
 prevent need of generating these entities on the fly.
 This would also mean we have to be a bit more sophisticated
 when combining new subsets into terrain mesh...
+Tried it. Not successful. No clear gains. But -- perhaps I
+did something wrong.
 
 """
 
@@ -27,52 +29,51 @@ window.exit_button.visible = False
 prevTime = time.time()
 
 scene.fog_color = color.rgb(0,200,211)
-scene.fog_density = 0.02
+# scene.fog_density = 0.02
+scene.fog_density=(10,200)
 
 grassStrokeTex = load_texture('grass_14.png')
 monoTex = load_texture('stroke_mono.png')
 
 def input(key):
+    global swirling
     if key == 'q' or key == 'escape':
         quit()
-    if key == 'g': generateSubswirl()
+    if key == 'g': swirling*=-1
 
 def update():
-    global prevZ, prevX, prevTime, subsets, terrainArea
+    global prevZ, prevX, prevTime, subsets, terrainLimit
+    global subArea
     if  abs(subject.z - prevZ) > 1 or \
         abs(subject.x - prevX) > 1:
-        generateShell()
+            generateShell()
+            
     
     # Safety net, in case of glitching through terrain.
     if subject.y < -amp:
         subject.y = floor((noise([subject.x/freq,
         subject.z/freq]))*amp)+3
+        subject.disable()   # To reset physics...
+        subject.enable()
 
-    if time.time() - prevTime > 0.5:
-        prevTime = time.time()
+    if time.time() - prevTime > 0.1:
         generateSubswirl()
-        if currentSubset == terrainArea-1:
+        # print(str(len(subsets)*subArea))
+        if len(subsets) == terrainLimit:
             finishTerrain()
+        prevTime = time.time()
 
-noise = PerlinNoise(octaves=3,seed=2021)
-amp = 32
+noise = PerlinNoise(octaves=2,seed=2021)
+amp = 42
 freq = 100
 terrain = Entity(model=None,collider=None)
-terrainArea = 100
-subWidth = 5
+terrainLimit = 600 # How many subsets before combining.
+subWidth = 6
 subArea = subWidth*subWidth
 subsets = []
 subCubes = []
 currentSubset = 0
-
-
-# Generate all subsets now, before running app, so that
-# we run more smoothly, not having to instantiate entities
-# on the fly.
-for i in range(terrainArea):
-    sub = Entity(model=None,collider=None)
-    sub.disable()
-    subsets.append(sub)
+swirling = 1 # Are we generating terrain?
 
 # For new position of subset.
 currentVec = 0
@@ -89,11 +90,12 @@ swirlVecs = [
 ]
 
 def generateSubswirl():
+    if swirling==-1: return
     global currentVec, iterations, changes, subArea
-    global subPos, toIterate, swirlVecs, currentSubset
+    global subPos, toIterate, swirlVecs
 
-    # sub = Entity(model=None,parent=terrain)
-    # subsets.append(sub)
+    sub = Entity(model=None,parent=terrain)
+    subsets.append(sub)
 
     # Translate position of subset, according to
     # current vector.
@@ -103,17 +105,12 @@ def generateSubswirl():
     for i in range(subArea):
         x = subCubes[i].x = floor(i/subWidth) + subPos.x
         z = subCubes[i].z = floor(i%subWidth) + subPos.y
-        y = subCubes[i].y = floor((noise([x/freq,z/freq]))*amp)
-        subCubes[i].parent = subsets[currentSubset]
+        y = subCubes[i].y = ((noise([x/freq,z/freq]))*amp)
+        subCubes[i].parent = subsets[-1]
         r = randrange(142,244)
         subCubes[i].color = color.rgb(r,0,0)
-        subCubes[i].visible = False
     
-    subsets[currentSubset].texture = monoTex
-    subsets[currentSubset].combine(auto_destroy=False)
-    subsets[currentSubset].parent = terrain
-
-    currentSubset+=1
+    subsets[-1].combine(auto_destroy=False)
 
     # Co-ordinate new vector by iteration around swirl.
     iterations+=1
@@ -133,21 +130,20 @@ for i in range(subArea):
     bud.disable()
     subCubes.append(bud)
     
-
-terrainFinished = False
-terrain.texture = grassStrokeTex
+terrain.texture = monoTex
 def finishTerrain():
-    global terrainFinished, subsets, terrain, subCubes
-    global currentSubset
+    global subsets, terrain, currentSubset
+
+    # Since subsets will be destroyed, reparent subcubes.
+    for sc in subCubes:
+        sc.parent = scene
+
+    terrain.combine()
     
-    # for sc in subCubes:
-    #     sc.parent = scene
-    
-    terrain.combine(auto_destroy=False)
-    
-    # for ss in subsets:
-        # ss.model = None
-        # ss.parent = scene
+    # Make sure our subset list is empty, since its
+    # entities have just been destroyed.
+    subsets = []
+
     currentSubset=0
     
 shellies = []
@@ -164,21 +160,26 @@ def generateShell():
                             subject.x - 0.5*shellWidth)
         z = shellies[i].z = floor((i%shellWidth) + 
                             subject.z - 0.5*shellWidth)
-        shellies[i].y = floor((noise([x/freq,z/freq]))*amp)
+        shellies[i].y = ((noise([x/freq,z/freq]))*amp)
 
 subject = FirstPersonController()
 subject.cursor.visible = False
 subject.gravity = 0.5
-subject.x = subject.z = 0
+subject.x = subject.z = subWidth*0.5
 subject.y = amp+3
 prevZ = subject.z
 prevX = subject.x
+
+disp = Entity(model='quad',parent=camera.ui,
+scale_y=0.1,scale_x=1,y=-0.5)
+
 
 chickenModel = load_model('chicken.obj')
 vincent = Entity(model=chickenModel,scale=1,
                 x=22,z=16,y=7.1,
                 texture='chicken.png',
-                double_sided=True)
+                double_sided=True,
+                collider='mesh')
 
 generateShell()
 
