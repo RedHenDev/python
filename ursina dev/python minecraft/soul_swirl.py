290aafdc44dc37506e9f3057ad93ebdbd1a861b2
RedHenDev <neobn@hotmail.com>
2021-07-14 15:26:10 +0100

    optimising soulSwirl working; next, manage larger terrain; any other opts?

--- ursina dev/python minecraft/soul_swirl.py
+++ ursina dev/python minecraft/soul_swirl.py
@@ -35,7 +35,7 @@ is not surrounded by enough terrain.
 from random import randrange
 from ursina import *  
 # from ursina.prefabs.first_person_controller import FirstPersonController
-from numpy import floor
+from numpy import csingle, floor
 from numpy import abs
 from numpy import cos
 from numpy import sin
@@ -59,25 +59,28 @@ grassStrokeTex = load_texture('grass_14.png')
 monoTex = load_texture('stroke_mono.png')
 
 def input(key):
-    global swirling
+    global swirling, canSwirl
     if key == 'q' or key == 'escape':
         quit()
-    if key == 'g': swirling*=-1
+    if key == 'g': 
+        swirling*=-1
+        canSwirl*=-1
 
 def update():
-    global prevZ, prevX, prevTime, subsets, terrainLimit
-    global subArea, subSpeed, toIterate, subPos
-    global changes, iterations, swirling, comboTip
+    global prevZ, prevX, prevTime, subsets
+    global subSpeed, perCycle
+    global swirling, comboTip
     global rad
             
     generateShell()
 
-    if  abs(subject.z - prevZ) >= 4 or \
-        abs(subject.x - prevX) >= 4:
+    if  abs(subject.z - prevZ) >= 0.9 or \
+        abs(subject.x - prevX) >= 0.9:
         prevZ = subject.z
         prevX = subject.x
         # Reset swirling settings...?
-        rad = 0
+        rad = 1
+        swirling=1*canSwirl
 
     # Safety net, in case of glitching through terrain.
     if subject.y < -amp:
@@ -86,7 +89,8 @@ def update():
         subject.land()
 
     if time.time() - prevTime > subSpeed:
-        genSub()
+        for i in range(perCycle):
+            genSub()
         prevTime = time.time()
 
 noise = PerlinNoise(octaves=24,seed=99)
@@ -99,28 +103,28 @@ comboTip = Tooltip('<pink>Warning! Combining ' +
                     str(terrainLimit) + 'subsets of #' + 
                     str(len(terrains)) + ' terrain!')
 comboTip.enabled=False
-subWidth = 6
-subSpeed = 0
-subArea = subWidth*subWidth
 subsets = []
+numSubCubes = 32 # Number of cubes per subset.
+subSpeed = 0 # How long before new cubes added to terrain?
+perCycle = 8    # How many cubes positioned per update?
+radLimit = 64   # How far a radius before swirling off?
+cs = 0 # Current subset.
+bsf = 0 # Blocks so far.
+for i in range(999):
+    bud = Entity(model=None)
+    bud.texture=grassStrokeTex
+    bud.disable()
+    subsets.append(bud)
 subCubes = []
+for i in range(numSubCubes):
+    bud = Entity(model='cube')
+    bud.disable()
+    subCubes.append(bud)
+
 currentSubset = 0
 swirling = 1 # Are we generating terrain?
+canSwirl = 1 # Are we allowed to turn on swirling?
 
-
-# For new position of subset.
-currentVec = 0
-iterations = 0
-toIterate = 1
-changes = -1
-subPos = Vec2(0,0)
-swirlVecs = [
-    Vec2(0,0),
-    Vec2(0,1),
-    Vec2(1,0),
-    Vec2(0,-1),
-    Vec2(-1,0)
-]
 # Dictionary for recording whether terrain blocks exist
 # at location specified in key.
 subDic = {}
@@ -131,39 +135,56 @@ def getPerlin(_x,_z):
 
 rad = 0
 theta = 0
+thetaDir = 1 # Which way are we swirling?
+swished = 0  # When swished twice, we increment rad.
 def genSub():
-    global theta, rad, swirling
+    global theta, rad, swirling, radLimit
+    global cs, bsf, numSubCubes
+    global swished, thetaDir
     if swirling==-1: return
     # Is there already a terrain block here?
     x = round(subject.x + rad * cos(radians(theta)))
     z = round(subject.z + rad * sin(radians(theta)))
-    if subDic.get(str(x)+str(z))==None:
-        bud = Entity(   model='cube',
-                    texture=grassStrokeTex)
+    if subDic.get('x'+str(x)+'z'+str(z))!='i':
+        subCubes[bsf].enable()
+        subCubes[bsf].parent=subsets[cs]
         subDic['x'+str(x)+'z'+str(z)]='i'
-        print(subDic)
-        bud.x = x
-        bud.z = z
-        y = bud.y = getPerlin(x,z)
-        bud.color = color.rgb(0,
-                    nMap(y,0,amp,64,255),200)
-    
+        # print(subDic)
+        subCubes[bsf].x = x
+        subCubes[bsf].z = z
+        y = subCubes[bsf].y = getPerlin(x,z)
+        subCubes[bsf].color = color.rgb(0,
+                              nMap(y,0,amp,64,255),200)
+
+        # Time to combine cubes into subset?
+        # NB. this will 'destroy' its child cubes,
+        # so we need to start bsf again at zero.
+        # And, to iterate to next subset.
+        subCubes[bsf].disable()
+        bsf+=1
+        if bsf==numSubCubes:
+            subsets[cs].combine(auto_destroy=False)
+            subsets[cs].enable()
+            cs+=1
+            bsf=0
+    else:
+        pass
+        #print('already block at ' + 'x'+str(x)+'z'+str(z))
+        #rad+=0.01
+        
     # Swirl to next terrain position.
-    theta-=90/((rad+1)*3.14)
+    theta-=(90/((rad+1)*3.14))*thetaDir
     if theta <= -360: 
         theta = 0
         rad += 1
-
-
-
-
-
+        if rad > radLimit:
+            swirling=-1
+            rad=1
     
 shellies = []
 shellWidth = 3
 for i in range(shellWidth*shellWidth):
     bud = Entity(model='cube',scale_y=4,collider='box')
-
     bud.visible=False
     shellies.append(bud)
 
@@ -179,9 +200,9 @@ def generateShell():
 subject = SubjectiveController()
 subject.cursor.visible = False
 subject.gravity = 0.5
-subject.speed = 4
+subject.speed = 3
 subject.step_height = 1
-subject.x = subject.z = subWidth*0.5
+subject.x = subject.z = 0
 subject.y = amp+6
 prevZ = subject.z
 prevX = subject.x
