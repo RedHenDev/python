3bda6a4dfc3a4312ee2b5057bed8cbb26c0ce2ad
RedHenDev <neobn@hotmail.com>
2021-10-23 15:51:17 +0100

    reorganised modules
    terrain loading is now working in new module organisation.
    
    pycraft_mesh_dev (main) imports rh_gen_terrain

--- ursina dev/txt_to_ent/pycraft_mesh_dev.py
+++ ursina dev/txt_to_ent/pycraft_mesh_dev.py
@@ -1,76 +1,23 @@
-from random import randint
 from ursina import *
-from perlin_noise import PerlinNoise
 from ursina.prefabs.first_person_controller import FirstPersonController
-from nMap import nMap
-from file_byte import load, save
+from rh_gen_terrain import loadMap
 
 app = Ursina()
 
-noise = PerlinNoise(octaves=1,seed=int(99))
-map_name = 'terrain_1.map'
-terrainSize = 0 # To be derived from loaded map data :)
-td = {} # Terrain dictionary.
-# quad = load_model('stretch_hex.obj')
-# dungeon = Entity(model=Mesh(), texture='grass_64_hex_tex.png')
-quad = load_model('block.obj')
-dungeon = Entity(model=Mesh(), texture='block_texture.png')
-dungeon2 = Entity(model=Mesh(), texture='block_texture2.png')
-model = dungeon.model
-model2 = dungeon2.model
-
-def genPerlin(_x, _z):
-    y = 0
-    freq = 64
-    amp = 32      
-    y += ((noise([_x/freq,_z/freq]))*amp)
-    freq = 64
-    amp = 21
-    y += ((noise([_x/freq,_z/freq]))*amp)
-    return y
-
-def urizen(_map_name, load_terrain=True):
-    global td
-    if load_terrain==True:
-        td = load(_map_name)
-    # Derive terrain size. Position subject.
-    terrainSize = floor(math.sqrt(len(td)))
-    subject.z = terrainSize*0.5
-    subject.x = terrainSize*0.5
-    for z in range(terrainSize):
-        for x in range(terrainSize):
-            if load_terrain==False:
-                y = floor(genPerlin(x,z))
-                y += sin(x)*0.5
-                td[str(x)+'_'+str(z)] = y    
-            else:
-                y = td.get(str(x)+'_'+str(z))
-                cc = nMap(y,-32,32,0.32,0.84)
-                cc += randint(1,100)/100
-                if randint(1,2)!=1:
-                    model.colors.extend((Vec4(cc,cc,cc,1),) * len(quad.vertices))
-                    model.vertices.extend([Vec3(x,y,z)+v for v in quad.vertices])
-                else:
-                    model2.colors.extend((Vec4(cc,cc,cc,1),) * len(quad.vertices))
-                    model2.vertices.extend([Vec3(x,y,z)+v for v in quad.vertices])
-    if load_terrain==False:
-        save(_map_name, td)
-    else:
-        model.uvs = (quad.uvs) * (terrainSize * terrainSize)
-        model.generate()
-        model2.uvs = (quad.uvs) * (terrainSize * terrainSize)
-        model2.generate()
-
 subject = FirstPersonController()
-subject.y = 100
+subject.y = 32
+subject.x = 32
+subject.z = 32
 subject.gravity = 0.0
 subject.cursor.visible=False
 window.color=color.cyan
 scene.fog_color = color.cyan
 scene.fog_density = 0.01
 
-# False saves terrain to file; True plays!
-urizen(map_name,True)
+map_name = 'terrain_1.map'
+td = {} # Terrain dictionary.
+td = loadMap(map_name)
+"""
 minimap_scale = 0.02
 uri = duplicate(dungeon)
 uri.texture='white_cube'
@@ -87,9 +34,15 @@ mark = Entity(model='sphere',color=color.red)
 mark.parent=uri
 mark.scale *= 8
 mark.always_on_top=True
+"""
 
-counter=0
+# def input(key):
+#     if key=='g':
+#         # genTerrain()
+
+# counter=0
 def update():
+    """
     global counter
     counter+=1
     # Minimap.
@@ -103,10 +56,11 @@ def update():
                         Vec3(-adjustPos,0,-adjustPos)*minimap_scale+
                         subject.position*minimap_scale+
                         subject.down*2)
+    """
     try:
         target_y = 2 + td.get(str(floor(subject.x))+'_'+str(floor(subject.z)))
         subject.y = lerp(subject.y, target_y, 0.1)
     except: 
-        subject.x = adjustPos
-        subject.z = adjustPos
+        subject.x = 32
+        subject.z = 32
 app.run()
---
+++ ursina dev/txt_to_ent/rh_gen_terrain.py
@@ -0,0 +1,88 @@
+"""
+Generate terrain via mesh.
+rh_gen_terrain.py
+
+Perhaps all we want to do is
+work out the y heights of a given
+co-ordinate, or range?
+Then, the main module can worry about
+actually building it?
+
+Well -- what objects 'would' we need?
+
+We'd need the terrain dictionary (td), and the
+collection of terrain entities (dungeon) and their model
+variables (model), the quad, and terrainSize?
+
+Idea1: we could append these to a list and pass that one
+list in and out of the function here? I.e. to avoid all
+class complications?
+
+Idea2: we only need td in main module...so perhaps
+all that this function needs access to is td? I.e.
+all the terrain entity objects will belong to this
+module itself?
+"""
+from ursina import *
+from perlin_noise import PerlinNoise
+from file_byte import load
+from nMap import nMap
+from random import randint
+from numpy import floor, sqrt
+
+
+
+def genPerlin(_x, _z):
+    _seed = (ord('l'))
+    noise1 = PerlinNoise(octaves=1,seed=_seed)
+    noise2 = PerlinNoise(octaves=3,seed=_seed)
+    noise3 = PerlinNoise(octaves=6,seed=_seed)
+    noise4 = PerlinNoise(octaves=12,seed=_seed)
+
+    y = 0
+    freq = 128
+    amp = 32 
+    y += ((noise1([_x/freq,_z/freq]))*amp)
+    amp = 16
+    y += ((noise2([_x/freq,_z/freq]))*amp)
+    amp = 8
+    y += ((noise3([_x/freq,_z/freq]))*amp)
+    amp = 1
+    y += ((noise4([_x/freq,_z/freq]))*amp)
+
+    return y
+
+def loadMap(_map_name):
+    terrainSize = 0 # To be derived from loaded map data :)
+    block = load_model('block.obj')
+    dungeon = Entity(model=Mesh(),texture='block_texture.png')
+    dungeon2 = Entity(model=Mesh(),texture='block_texture2.png')
+    model = dungeon.model
+    model2 = dungeon2.model
+
+    _td={}
+    _td = load(_map_name)
+    # Derive terrain size. Position subject.
+    terrainSize = int(floor(sqrt(len(_td))))
+    for z in range(terrainSize):
+        for x in range(terrainSize):
+            
+            # y = floor(genPerlin(x,z))
+            # td[str(x)+'_'+str(z)] = y    
+            
+            y = _td.get(str(x)+'_'+str(z))
+            cc = nMap(y,-32,32,0.32,0.84)
+            cc += randint(1,100)/100
+            if randint(1,2)!=1:
+                model.colors.extend((Vec4(cc,cc,cc,1),) * len(block.vertices))
+                model.vertices.extend([Vec3(x,y,z)+v for v in block.vertices])
+            else:
+                model2.colors.extend((Vec4(cc,cc,cc,1),) * len(block.vertices))
+                model2.vertices.extend([Vec3(x,y,z)+v for v in block.vertices])
+
+    model.uvs = (block.uvs) * (terrainSize * terrainSize)
+    model.generate()
+    model2.uvs = (block.uvs) * (terrainSize * terrainSize)
+    model2.generate()
+
+    return _td
