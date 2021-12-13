28cfbc85e68e646c851bf9673192b381805e44f4
RedHenDev <neobn@hotmail.com>
2021-12-13 23:23:22 +0000

    better mesh coloration

--- sorenWorld/sorenWorld.py
+++ sorenWorld/sorenWorld.py
@@ -4,7 +4,7 @@ from random import random
 
 app = Ursina()
 
-scene.color=color.rgb(0,200,255)
+Sky(color=color.rgb(0,200,255))
 
 me = FirstPersonController()
 me.y = 100
@@ -13,19 +13,18 @@ me.x = me.z = 16
 mod = load_model('cube')
 terrain = Entity(model=Mesh(), texture='grey_noise.png')
 model = terrain.model
-terrain.color=color.green
+# terrain.color=color.green
 
-texture = load_texture('map_3.png')
-for z in range(64):
-    for x in range(64):
+texture = load_texture('map_4.png')
+if texture.width > 64: terrainWidth = 64
+else: terrainWidth = texture.width
+for z in range(terrainWidth):
+    for x in range(terrainWidth):
         px = texture.get_pixel(x,z)
 
         model.vertices.extend([Vec3(x,floor(px.v*10),z)+
                                 v for v in mod.vertices])
-
-        # Decide random tint for colour of block :)
-        c = random()-0.5
-        model.colors.extend( (Vec4(1-c,1-c,1-c,1),)*
+        model.colors.extend( (Vec4(px.r,px.g,px.b,1),)*
                                 len(mod.vertices))
         model.uvs.extend([Vec2(0,0) + u for u in mod.uvs])
 model.generate()
