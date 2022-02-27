1cfcaec94b0c6dfe4a2f59c340dba4873db986c9
RedHenDev <neobn@hotmail.com>
2022-02-27 16:32:11 +0000

    inventory snapping system partially working

--- ursina dev/python minecraft/meshcraft current/inv_system.py
+++ ursina dev/python minecraft/meshcraft current/inv_system.py
@@ -25,58 +25,60 @@ class item_panel(Entity):
     def __init__(this,cols=9,rows=3):
         super().__init__()
         this.model='quad'
-        this.ratio=rows/cols
-        this.cols=cols
-        this.rows=rows 
-        # NB y is 1/3 of x. I.e. rows v cols.
-        this.scale=0.8
-        this.scale_y=this.ratio*0.8
-        this.parent=camera.ui
-        this.trays=[Vec2(x,y) for x in range(this.cols) for y in range(this.rows)]
         this.texture='white_box.png'
+        this.parent=camera.ui
+        this.cols=cols
+        this.rows=rows
         this.texture_scale=(this.cols,this.rows)
+        this.scale=(this.cols/10,this.rows/10)
+        this.origin=(-0.5,0.5)
+        this.x=this.scale_x*-0.5
+        this.y=this.scale_y*0.5
+        this.trays=[Vec2(x,y) for x in range(this.cols) for y in range(this.rows)]
+        
+        
     
     def update(this):
         pass
 
 ite=item_panel()
 
-class hotspot(Entity):
-    def __init__(this):
-        super().__init__()
-        this.model='quad'
-        this.parent=camera.ui
-        this.color=color.dark_gray
-        # Why 10 here instead of 9? Weird.
-        this.scale=hotbar.scale_x/10.3
-        this.y=-0.5+(hotbar.scale_y*0.5)
-
 class item_icon(Draggable):
     def __init__(this,blockType='grass'):
         super().__init__()
         this.model='quad'
         this.parent=ite # Let's try this...
+        this.scale_x=1/(this.parent.texture_scale[0]*1.2)
+        this.scale_y=1/(this.parent.texture_scale[1]*1.2)
+        this.origin=(0.6,-0.6)
+        
         this.blockType=blockType
-
         this.color=color.white
-        this.scale_x=(this.parent.scale_x)/9
-        this.scale_y=(this.parent.scale_y)
-        this.origin=(   -5,
-                        0.8*this.parent.ratio*5)
-        # this.origin=(   -this.parent.scale_x*6.25,
-        #                 this.parent.scale_y*4.7)
+
         this.texture='texture_atlas_3.png'
-        this.texture_scale*=64/this.texture.width
-        # (32/(this.texture.width))  
+        this.texture_scale*=64/this.texture.width  
         this.setup_texture()
 
     def drop(this):
-        # print(this.x)
-        sv = str(this.x)[:3]
-        this.x = 0.11*float(sv)*10
-        sv = str(this.y/3)[:4]
-        this.y = 0.33*float(sv)*10
-        print(this.y)
+        print(f"x={this.x},y={this.y}")
+        # this.x=int(this.x*10)/10
+        # this.y=int(this.y*100)/100
+        """
+        x=-0.00010246038436889648,y=-0.0009403526782989502
+        x=0.11149701476097107,y=-0.3401150405406952
+        x=0.2226986289024353,y=-0.6690537929534912
+        """
+        sv = str(abs(this.x))[2]
+        this.x = 0.111*float(sv)
+
+        sv = str(abs(this.y))[2]
+        sv = int(round(int(sv))/3)
+        print(sv)
+        this.y= -sv * 0.33
+        # if sv == 0: this.y=0
+        # elif sv==1: this.y=-0.34
+        # else: this.y=-0.67
+        
 
     def setup_texture(this):
         uu=minerals[this.blockType][0]
@@ -86,6 +88,7 @@ class item_icon(Draggable):
         # print(len(cb.uvs))
         # OK this isn't right -- but gives ok texture.
         del cb[:-33]
+        print(cb)
         this.model.uvs = [Vec2(uu,uv) + u for u in cb]
         this.model.generate()
         this.rotation_z=180
@@ -97,23 +100,6 @@ class item_icon(Draggable):
             this.color=minerals[this.blockType][2]
 
 test_item=item_icon()
-
-# Test hotspots.
-# hs=[]
-# for i in range(9):
-#     e = hotspot()
-#     e.x = (-4.5*e.scale_x) + ((e.scale_x+0.01) * i)
-#     e.scale*=1.2
-#     hs.append(e)
-# hs[0].color=color.white
-# gs = []
-# for i in range(9):
-#     e = item_icon(mins[i%len(mins)])
-#     e.x = (-4.5*e.scale_x) + ((e.scale_x+0.01) * i)
-#     e.setup_color()
-#     e.scale*=1.08
-#     e.lock_x=e.lock_y=1
-#     gs.append(e)
 
 def inv_input(key,subject,mouse):
     global gs
