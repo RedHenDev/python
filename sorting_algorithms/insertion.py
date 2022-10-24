c202c52e28d28d6ff632395ea20c468da6108fa4
RedHenDev <neobn@hotmail.com>
2022-10-24 11:19:00 +0100

    insertion sort works

--- sorting_algorithms/insertion.py
+++ sorting_algorithms/insertion.py
@@ -1,31 +1,29 @@
 # Insertion sort.
+# Would be cool to map index to position in a line
+# and value to size, e.g. of a line of pumpkins.
 
-li = [2,1,3,5,4,0,2,7,5,3,9]
+li = [2,1,3,5,4,0,2,7,5,3,0]
 
 print(li)
-
+from copy import copy
 def insSort(_list):
-    iS=0 # Sorted index, furthest to right.
-    l=len(_list)
 
-    ol=_list[0:iS]
-    ul=_list[iS+1:l]
-
-    for i in range(len(ul)):
-        for j in range(len(ol)):
-            if ul[i] < ol[j]:
-                ol.insert(j,ul[i])
-                ul.remove(i)
-                break
-        
-
-
+    ol=copy([_list[0]])
+    ul=copy(_list[1:])
+    print(ol," ",ul)
+    inserted=False
+    for iU in range(len(ul)):
+        for iO in range(len(ol)):
+            if ul[iU] <= ol[iO]:
+                print('Inserting ', ul[iU])
+                ol.insert(iO,ul[iU])
+                inserted=True
+                break 
+        if not inserted:
+            ol.append(ul[iU])
+            print('Appending ', ul[iU])
+        inserted=False
+    
     return ol
 
 print("Ordered list, ",insSort(li))
-
-
-
-
-
-
