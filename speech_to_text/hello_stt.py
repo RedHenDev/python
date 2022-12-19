55161dcc9450fc0c9d2dd3fb83d7bec4f3841cc2
RedHenDev <neobn@hotmail.com>
2022-12-19 22:43:05 +0000

    modularised mic_listen and robot_listen

--- speech_to_text/hello_stt.py
+++ speech_to_text/hello_stt.py
@@ -17,40 +17,12 @@ except:
     arm = None
 
 print(sr.__version__)
-print(sr.Microphone.list_microphone_names())
+# print(sr.Microphone.list_microphone_names())
 
 r = sr.Recognizer()
 # index 0 - i.e. default mic.
 # NB Microphone is a context manager.
 mic = sr.Microphone(device_index=0)
-
-# Now let's try to listen to something...
-# NB duration should be no less than 0.5.
-with mic as source:
-    print('Wait one second please...')
-    r.adjust_for_ambient_noise(source,duration=0.75)
-    print('Listening...please speak:')
-    audio=r.listen(source)
-
-# Set up a response dictionary.
-response = {
-    "success": True,
-    "error": None,
-    "transcription": None
-}
-
-# Try to recognise speech in the recording.
-# If a RequestError or UnknownValueError exception 
-# is caught, update the response object.
-try:
-    response["transcription"] = r.recognize_google(audio)
-except sr.RequestError:
-    # API unreachable or unresponsive.
-    response["success"] = False
-    response["error"] = "API unavailable"
-except sr.UnknownValueError:
-    # Speech garbled.
-    response["error"] = "Unable to recognise speech"
 
 # def contains_word(s, w):
 #     return (' ' + w + ' ') in (' ' + s + ' ')
@@ -59,13 +31,63 @@ def findWholeWord(w):
     # This returns a Pattern object.
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
-if arm is None or arm == None:
-    print(response["transcription"])
-    print('But no robot connected.')
-else:
-    if findWholeWord('light')(str(response["transcription"])):
+def mic_listen():
+    # Now let's try to listen to something...
+    # NB duration should be no less than 0.5.
+    with mic as source:
+        print('Wait one second please...')
+        r.adjust_for_ambient_noise(source,duration=0.75)
+        print('Listening...please speak:')
+        audio=r.listen(source)
+
+    # Set up a response dictionary.
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # Try to recognise speech in the recording.
+    # If a RequestError or UnknownValueError exception 
+    # is caught, update the response object.
+    try:
+        response["transcription"] = r.recognize_google(audio)
+        return str(response["transcription"])
+    except sr.RequestError:
+        # API unreachable or unresponsive.
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # Speech garbled.
+        response["error"] = "Unable to recognise speech"
+    return 'error'
+
+def robot_listen(_message):
+    if arm is None or arm == None:
+        print(_message)
+        print('But no robot connected.')
+        return
+        
+    print(_message)
+    if findWholeWord('light')(_message):
         arm.move(usb_arm.LedOn)
-    if findWholeWord('yes')(str(response["transcription"])):
+    if findWholeWord('yes')(_message):
         for i in range(3):
             arm.move(usb_arm.WristDown,0.4)
             arm.move(usb_arm.WristUp,0.4)
+    if findWholeWord('right')(_message):
+        arm.move(usb_arm.BaseClockWise,2)
+    if findWholeWord('left')(_message):
+        arm.move(usb_arm.BaseCtrClockWise,2)
+    if findWholeWord('hello')(_message):
+        for i in range(3):
+            arm.move(usb_arm.BaseClockWise,0.4)
+            arm.move(usb_arm.BaseCtrClockWise,0.4)
+    # Just a little movement...
+    # To signal either completion or nothing understood.
+    arm.move(usb_arm.WristUp,0.1)
+    arm.move(usb_arm.WristDown,0.1)
+
+# Procedure: listen to mic. Then, pass result to
+# robot listen.
+robot_listen(mic_listen())
